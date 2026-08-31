ae557f60b6b2934fdf5c35d9b2c8ba26ddeae1f0
marcusvbrangel <marcus.vbrangel@gmail.com>
2026-08-31 20:31:00 -0300

    feat: implemented the persistence of Order model

--- src/main/java/com/market/order/internal/application/OrderService.java
+++ src/main/java/com/market/order/internal/application/OrderService.java
@@ -1,6 +1,9 @@
 package com.market.order.internal.application;
 
 import com.market.order.OrderCreatedEvent;
+import com.market.order.internal.domain.Order;
+import com.market.order.internal.domain.OrderItem;
+import com.market.order.internal.infrastructure.persistence.OrderRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,22 +15,50 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
-    private final ApplicationEventPublisher event;
+    private final OrderRepository orderRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public OrderService(ApplicationEventPublisher event) {
-        this.event = event;
+    public OrderService(
+            OrderRepository orderRepository,
+            ApplicationEventPublisher eventPublisher
+    ) {
+        this.orderRepository = orderRepository;
+        this.eventPublisher = eventPublisher;
     }
 
     @Transactional
     public void createOrder(CreateOrderCommand command) {
+
         Objects.requireNonNull(command, "command é obrigatório");
 
-        var orderCreatedEvent = new OrderCreatedEvent(
+        var order = new Order(
                 UUID.randomUUID(),
-                Instant.now(),
                 command.customerId(),
                 command.paymentMethod(),
+                Instant.now(),
+                null,
                 command.items().stream()
+                        .map(item -> new OrderItem(
+                                UUID.randomUUID(),
+                                item.productId(),
+                                item.quantity()
+                        ))
+                        .toList()
+        );
+
+        var savedOrder = orderRepository.save(order);
+
+        publishOrderCreatedEvent(savedOrder);
+    }
+
+    private void publishOrderCreatedEvent(Order order) {
+
+        var orderCreatedEvent = new OrderCreatedEvent(
+                order.id(),
+                order.createdAt(),
+                order.customerId(),
+                order.paymentMethod(),
+                order.items().stream()
                         .map(item -> new OrderCreatedEvent.Item(
                                 item.productId(),
                                 item.quantity()
@@ -35,6 +66,6 @@ public class OrderService {
                         .toList()
         );
 
-        event.publishEvent(orderCreatedEvent);
+        eventPublisher.publishEvent(orderCreatedEvent);
     }
 }
