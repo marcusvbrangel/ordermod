2144169b6365b1026064849dba5cb96ca30671da
marcusvbrangel <marcus.vbrangel@gmail.com>
2026-08-31 15:54:49 -0300

    chore: initial commit

---
+++ src/main/java/com/market/OrdermodApplication.java
@@ -0,0 +1,13 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermodApplication.class, args);
+	}
+
+}
